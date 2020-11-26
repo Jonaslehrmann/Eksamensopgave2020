77ab80216849059bc0aa6b3dba1f736f413f1d0a
Jonas Lehrmann <jonaslehrmann@Jonass-MacBook-Pro.local>
2020-11-26 13:00:39 +0100

    commit2

---
+++ user.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(firstName, lastName, age,interest,match,image) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.interest = interest;
+        this.match = match;
+        this.image = image;
+    }
+}
+module.exports = User;
